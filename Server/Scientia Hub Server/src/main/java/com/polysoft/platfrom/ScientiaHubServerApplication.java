7d5e12dcac33aeeea84ef19258f512726e3b7227
Ahmed <ahmed.ghorbel1@esprit.tn>
2024-02-26 17:11:20 +0100

    Server Client setup

---
+++ Server/Scientia Hub Server/src/main/java/com/polysoft/platfrom/ScientiaHubServerApplication.java
@@ -0,0 +1,13 @@
+package com.polysoft.platfrom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScientiaHubServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScientiaHubServerApplication.class, args);
+    }
+
+}
